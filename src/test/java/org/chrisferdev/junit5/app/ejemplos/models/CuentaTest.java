5209709e215d29cc5efd2ea039bfed02fb361e92
Christian <ferchris82@hotmail.com>
2024-01-19 16:58:32 -0500

    Versión 0.1

--- src/test/java/org/chrisferdev/junit5/app/ejemplos/models/CuentaTest.java
+++ src/test/java/org/chrisferdev/junit5/app/ejemplos/models/CuentaTest.java
@@ -25,4 +25,13 @@ class CuentaTest {
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
     }
+
+    @Test
+    void testReferenciaCuenta() {
+        Cuenta cuenta = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+        Cuenta cuenta2 = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+
+        //assertNotEquals(cuenta2, cuenta);
+        assertEquals(cuenta2, cuenta);
+    }
 }
