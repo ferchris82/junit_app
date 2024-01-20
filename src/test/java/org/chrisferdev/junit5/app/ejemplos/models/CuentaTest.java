4136a78662c7d6f7bc7ac13c8d536df87bac61b7
Christian <ferchris82@hotmail.com>
2024-01-19 20:23:53 -0500

    Versión 0.1

--- src/test/java/org/chrisferdev/junit5/app/ejemplos/models/CuentaTest.java
+++ src/test/java/org/chrisferdev/junit5/app/ejemplos/models/CuentaTest.java
@@ -65,4 +65,41 @@ class CuentaTest {
         String esperado = "Dinero Insuficiente";
         assertEquals(esperado, actual);
     }
+
+    @Test
+    void testTransferirDineroCuentas() {
+        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("Andres", new BigDecimal("1500.8989"));
+
+        Banco banco = new Banco();
+        banco.setNombre("Banco del Estado");
+        banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
+        assertEquals("1000.8989", cuenta2.getSaldo().toPlainString());
+        assertEquals("3000", cuenta1.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testRelacionBancosCuentas() {
+        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("Andres", new BigDecimal("1500.8989"));
+
+        Banco banco = new Banco();
+        banco.addCuentas(cuenta1);
+        banco.addCuentas(cuenta2);
+
+        banco.setNombre("Banco del Estado");
+        banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
+        assertEquals("1000.8989", cuenta2.getSaldo().toPlainString());
+        assertEquals("3000", cuenta1.getSaldo().toPlainString());
+
+        assertEquals(2, banco.getCuentas().size());
+        assertEquals("Banco del Estado", cuenta1.getBanco().getNombre());
+        assertEquals("Andres", banco.getCuentas().stream()
+                .filter(c -> c.getPersona().equals("Andres"))
+                .findFirst()
+                .get().getPersona());
+
+        assertTrue(banco.getCuentas().stream()
+                .anyMatch(c -> c.getPersona().equals("John Doe")));
+    }
 }
