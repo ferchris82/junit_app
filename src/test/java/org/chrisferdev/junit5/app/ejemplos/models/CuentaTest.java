00302eec3294cadab98a0c5e4c83d515691d0ffb
Christian <ferchris82@hotmail.com>
2024-01-22 13:43:41 -0500

    Versión 1.1

--- src/test/java/org/chrisferdev/junit5/app/ejemplos/models/CuentaTest.java
+++ src/test/java/org/chrisferdev/junit5/app/ejemplos/models/CuentaTest.java
@@ -2,16 +2,19 @@ package org.chrisferdev.junit5.app.ejemplos.models;
 
 import org.chrisferdev.junit5.app.ejemplos.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
 import java.math.BigDecimal;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CuentaTest {
     Cuenta cuenta;
 
     @BeforeEach
-    void initMetodoTest(){
+    void initMetodoTest() {
         this.cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
         System.out.println("iniciando el metodo.");
     }
@@ -19,6 +22,16 @@ class CuentaTest {
     @AfterEach
     void tearDown() {
         System.out.println("finalizando el método de prueba.");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("inicializando el test");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("finalizando el test");
     }
 
     @Test
@@ -36,7 +49,6 @@ class CuentaTest {
     @Test
     @DisplayName("probando el saldo de la cuenta corriente, que no sea null, mayor que cero, valor esperado.")
     void testSaldoCuenta() {
-        cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
         assertNotNull(cuenta.getSaldo());
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
@@ -108,11 +120,11 @@ class CuentaTest {
         banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
 
         assertAll(() -> assertEquals("1000.8989", cuenta2.getSaldo().toPlainString(),
-                                    () -> "el valor del saldo de la cuenta2 no es el esperado."),
+                        () -> "el valor del saldo de la cuenta2 no es el esperado."),
                 () -> assertEquals("3000", cuenta1.getSaldo().toPlainString(),
-                                    () -> "el valor del saldo de la cuenta1 no es el esperado."),
+                        () -> "el valor del saldo de la cuenta1 no es el esperado."),
                 () -> assertEquals(2, banco.getCuentas().size(),
-                                    () -> "el banco no tiene las cuentas esperadas."),
+                        () -> "el banco no tiene las cuentas esperadas."),
                 () -> assertEquals("Banco del Estado", cuenta1.getBanco().getNombre()),
                 () -> assertEquals("Andres", banco.getCuentas().stream()
                         .filter(c -> c.getPersona().equals("Andres"))
@@ -120,5 +132,66 @@ class CuentaTest {
                         .get().getPersona()),
                 () -> assertTrue(banco.getCuentas().stream()
                         .anyMatch(c -> c.getPersona().equals("John Doe"))));
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testSoloWindows(){
+    }
+
+    @Test
+    @EnabledOnOs({OS.LINUX, OS.MAC})
+    void testSoloLinuxMac(){
+    }
+
+    @Test
+    @DisabledOnOs(OS.WINDOWS)
+    void testNoWindows(){
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    void soloJdk8(){
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_15)
+    void soloJDK15(){
+    }
+
+    @Test
+    @DisabledOnJre(JRE.JAVA_15)
+    void testNoJDK15(){
+    }
+
+    @Test
+    void imprimirSystemProperties(){
+        Properties properties = System.getProperties();
+        properties.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "java.version", matches = ".*17.*")
+    void testJavaVersion() {
+    }
+
+    @Test
+    @DisabledIfSystemProperty(named = "os.arch", matches = ".*32.*")
+    void testSolo64() {
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "os.arch", matches = ".*32.*")
+    void testNO64() {
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "user.name", matches = "ferch")
+    void testUserName() {
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "ENV", matches = "dev")
+    void testDev() {
     }
 }
